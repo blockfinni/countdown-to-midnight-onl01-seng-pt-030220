6f8d75f1639ef4f6942bceae31418d718b52d14f
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-03-13 17:49:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(count)
+  
+  count = x
+  
+  while x >= 1
+    puts "#{x} SECOND(S)!"
+    x = x - 1
+    
+  end 
+  
+ puts "Happy New Year"
+  
 end
+
+countdown
